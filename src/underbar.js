7cef3c05ff6d8a053d6e8c0f039148c65643ceb2
Ramin Taleghani <talegram@gmail.com>
2014-02-26 17:59:16 -0800

    Implementation of _.reduce function.

--- src/underbar.js
+++ src/underbar.js
@@ -157,6 +157,10 @@ var _ = { };
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(item) {
+      accumulator = iterator(accumulator, item);
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
